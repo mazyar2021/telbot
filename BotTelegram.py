3b35867ef56bc0ed073f13665cc073aeeacbeba3
mazyar2021 <82980539+mazyar2021@users.noreply.github.com>
2021-04-22 02:37:04 +0430

    Add files via upload

---
+++ BotTelegram.py
@@ -0,0 +1,7 @@
+import requests
+import time
+
+
+while True:
+        r = requests.get('https://api.telegram.org/bot1747227747:AAEs9fB4g_JPGw4sMOD2WBK7G_bWbbrKl-k/sendMessage?chat_id=1798139776&text=slam”, proxies=proxies')
+        print("|---> send!!! <---|")
